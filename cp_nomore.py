59edf06a7cd02559830816c7e14455a60090a7d8
Thomas Erickson <terickson@administrators-MacBook-Air.local>
2016-03-09 14:29:05 -0700

    working for everything but firefox

--- cp_nomore.py
+++ cp_nomore.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python
 import webbrowser
 import csv
+import numpy as np
 import sys
 import os
 import time
+import pyautogui
 from subprocess import call
 import random
-import pdb 
-import pyautogui
+import re
+from StringIO import StringIO
 
-file1 = open('easi_urls.txt', 'r')
-new = 1
-b_rowser = input('do you want to run this in safari, chrome, or firefox? Make sure to type your choice exactly as it appears \n')
+file1 = open('work.txt', 'r')
+#new = 1
+new_file = []
+#b_rowser = raw_input('do you want to run this in safari, chrome, or firefox? Make sure to type your choice exactly as it appears \n')
+b_rowser = 'firefox'
 if b_rowser == 'safari':
     b = webbrowser.get('safari')
+    bashCommand = "sudo killall 'Safari' "
 elif b_rowser == 'chrome':
-    b = webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
+    b = webbrowser.get()
+    bashCommand = "sudo killall 'google' "
 elif b_rowser == 'firefox':
     b = webbrowser.get('firefox')
-#b = webbrowser.get('safari')
+    #for row in file1:
+	#	new_file.append(row)
+
+
 for i in file1:
 	new = 1
 	b.open(i,new=0)
-	control = input('hit n for next test case or fail to indicate the test case as failed \n')
+	print(i)
+	control = raw_input('hit n for next test case or fail to indicate the test case as failed \n')
 	if control == 'n':
 		with open('results2.txt', 'a') as f:
     			f.write('pass,' + i)
